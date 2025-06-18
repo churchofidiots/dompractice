d9203f2b0cbe5835fdd3d2c0ffdba391bb6c96a0
churchofidiots <wolfnamiko@gmail.com>
2025-06-18 02:52:32 +0000

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 document.getElementById("greeting").textContent = "Hi There";
+document.getElementById("h1").textContent = "The Header";
+
+let items = document.getElementsByClassName("redclass");
+for (let i = 0; i < items.length; i++)
+{
+    items[i].textContent = "Text change";
+}
+
+const header = document.querySelector("h1");
+console.log(header);
+const red = document.querySelectorAll(".redclass");
+console.log(red);
+document.getElementById("click").addEventListener("click",function()
+{
+    alert("Clicked Button!");
+});
